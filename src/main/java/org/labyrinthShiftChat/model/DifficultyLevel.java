c21f42e238b46ee5fd74b8fb3a78d66f9bcd46ab
tittioAngel <matteo.angelucci00@gmail.com>
2025-03-18 10:59:10 +0100

    Aggiornato dimensioni labirinti

--- src/main/java/org/labyrinthShiftChat/model/DifficultyLevel.java
+++ src/main/java/org/labyrinthShiftChat/model/DifficultyLevel.java
@@ -1,10 +1,10 @@
 package org.labyrinthShiftChat.model;
 
 public enum DifficultyLevel {
-    EASY(5, 1, 1, 10),      // Labirinto 5x5, 1 nemico, 1 ostacolo, tempo 10 sec
-    MEDIUM(7, 3, 2, 15),    // Labirinto 7x7, 3 nemici, 2 ostacoli, tempo 15 sec
-    HARD(11, 5, 3, 20),     // Labirinto 11x11, 5 nemici, 3 ostacoli, tempo 20 sec
-    EXTREME(15, 10, 5, 25);  // Labirinto 15x15, 10 nemici, 5 ostacoli, tempo 5 sec
+    EASY(7, 1, 1, 5),      // Labirinto 7x7, 1 nemico, 1 ostacolo, tempo 10 sec
+    MEDIUM(9, 3, 2, 10),    // Labirinto 9x9 3 nemici, 2 ostacoli, tempo 15 sec
+    HARD(11, 3, 2, 15),     // Labirinto 11x11, 5 nemici, 3 ostacoli, tempo 20 sec
+    EXTREME(13, 4, 3, 20);  // Labirinto 13x13, 10 nemici, 5 ostacoli, tempo 5 sec
 
     private final int mazeSize;
     private final int enemyCount;
