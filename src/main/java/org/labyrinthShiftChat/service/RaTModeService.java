a4aa327eab1234492f2fee9cffe159804790bd54
MatteoDRC00 <matteo00drc@gmail.com>
2025-04-25 09:31:00 +0200

    modified RaTModeService

--- src/main/java/org/labyrinthShiftChat/service/RaTModeService.java
+++ src/main/java/org/labyrinthShiftChat/service/RaTModeService.java
@@ -1,47 +1,16 @@
 package org.labyrinthShiftChat.service;
 
-import org.labyrinthShiftChat.dao.GameSessionDAO;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.labyrinthShiftChat.model.*;
 import org.labyrinthShiftChat.singleton.GameSessionManager;
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class RaTModeService {
 
-    private final MazeService mazeService = new MazeService();
     private final GameSessionManager gameSessionManager = GameSessionManager.getInstance();
-    private final GameSessionDAO gameSessionDAO = new GameSessionDAO();
     private final ProfileService profileService = new ProfileService();
-
-
-    public RaTModeService() {}
-
-    public void createOrRegenerateMazeInGameSession(boolean isRegeneration, DifficultyLevel difficultyLevel) {
-        System.out.println(isRegeneration ? "🔄 Rigenerazione del minimaze in corso..." : "");
-
-        Maze maze = mazeService.generateRandomMaze(difficultyLevel);
-        Tile startTile = mazeService.getStartTile(maze);
-
-        GameSession gameSession;
-        if (isRegeneration) {
-            gameSession = gameSessionManager.getGameSession();
-            gameSession.getPlayer().setPosition(startTile.getX(), startTile.getY());
-            gameSession.setMaze(maze);
-            gameSession.setCurrentTile(startTile);
-            gameSession.setTimeRemaining(60);
-            gameSession.setGameMode(GameMode.RAT_MODE);
-            gameSessionDAO.update(gameSession);
-
-            System.out.println("✅ Nuovo minimaze pronto!");
-
-        } else {
-            gameSession = new GameSession(maze, startTile, 60);
-            saveGameSession(gameSession);
-            gameSessionManager.setGameSession(gameSession);
-        }
-    }
-
-    public void saveGameSession(GameSession gameSession) {
-        gameSessionDAO.save(gameSession);
-    }
 
     public void manageSaveCompletedRaT(DifficultyLevel difficultyLevel, int miniMazeCompleted) {
         Profile profile = gameSessionManager.getProfile();
